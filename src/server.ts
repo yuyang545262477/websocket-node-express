7404a04640c434ad72323f70cae7761e16170231
yuyang <yuyang545262477@gmail.com>
2022-07-12 22:18:53 +0800

    send date each time

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,27 @@ import * as WebSocket from "ws";
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
+let intervalId: NodeJS.Timer | null = null;
+const sendDataEachTime = () => {
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+    intervalId = setInterval(() => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`${new Date().toISOString()}`);
+            }
+        });
+    }, 500);
+};
 
 wss.on("connection", (ws: WebSocket) => {
     ws.on("message", (message: string) => {
         console.log(message);
         ws.send(`Hello, you sent -> ${message}`);
     });
+    sendDataEachTime();
     ws.send("Hi there, I am a WebSocket server");
 });
 
